175866507ba624f3698936ef8ce7b21978ad6cbe
Woolford-Totton <97439457+Woolford-Totton@users.noreply.github.com>
2022-01-28 11:50:38 +0000

    moved project to c drive downloads folder

--- Assets/Scripts/scr_Input.cs
+++ Assets/Scripts/scr_Input.cs
@@ -2,30 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr_Input : MonoBehaviour
+public class Scr_Input : MonoBehaviour
 {
     private float movespeed = 4.0f;
 
     private float Dashmultiplier = 1.5f;
 
+    private Vector3 StartCoords = new Vector3(0.0f,0.0f,0.0f);
+
+    private Vector3 PreviousPosition = new Vector3(0.0f, 0.0f, 0.0f);
+
     // Start is called before the first frame update
     void Start()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+
+        StartCoords = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        PreviousPosition = transform.position;
+
+
         float horizontalInput = Input.GetAxis("Horizontal");
         
         float verticalInput = -Input.GetAxis("Vertical");
 
-        float Dash = Input.GetAxis("Dash");
+       
 
-        var currentspd = movespeed * Time.deltaTime * (1+(Dash* Dashmultiplier));
+        var currentspd = movespeed * Time.deltaTime * (1);
 
         transform.Translate(horizontalInput * currentspd,  verticalInput * currentspd, 0);
     }
+
+    
 }
