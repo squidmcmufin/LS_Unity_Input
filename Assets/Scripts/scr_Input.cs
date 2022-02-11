3b809ab4cdf3bc081e03fdebe80d1c5c2e5930e1
squidmcmufin <16716@totton.ac.uk>
2022-02-11 12:35:21 +0000

    made a check point

--- Assets/Scripts/scr_Input.cs
+++ Assets/Scripts/scr_Input.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scr_Input : MonoBehaviour
 {
@@ -8,34 +9,65 @@ public class scr_Input : MonoBehaviour
 
     private float Dashmultiplier = 1.5f;
 
-    private Vector3 StartCoords = new Vector3(0.0f,0.0f,0.0f);
+    private Vector3 StartCoords = new Vector3(0.0f, 0.0f, 0.0f);
 
     private Vector3 PreviousPosition = new Vector3(0.0f, 0.0f, 0.0f);
 
-    // Start is called before the first frame update
+    public   bool grounded = false;
+
+    public Rigidbody rb;
+
+    public Vector3 jump;
+    public float jumpforce = 3f; 
+
+
     void Start()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
         StartCoords = transform.position;
+
+        jump = new Vector3 (0.0f, 2.0f, 0.0f);
+
+
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         PreviousPosition = transform.position;
 
 
         float horizontalInput = Input.GetAxis("Horizontal") + Input.GetAxis("Horizontal_Keyboard");
-        
-        float verticalInput = (-Input.GetAxis("Vertical")) + (- Input.GetAxis("Vertical_Keyboard"));
 
-       
+        float verticalInput = (-Input.GetAxis("Vertical")) + (-Input.GetAxis("Vertical_Keyboard"));
+
+
 
         var currentspd = movespeed * Time.deltaTime * (1);
 
-        transform.Translate(horizontalInput * currentspd,  verticalInput * currentspd, 0);
+        transform.Translate(horizontalInput * currentspd, verticalInput * currentspd, 0);
+
+
+        if (Input.GetKey("a") | Input.GetKey("left"))
+        {
+
+
+        }
+
+         
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded){
+            rb.AddForce(jump * jumpforce, ForceMode.Impulse);
+            grounded = false;
+        }
     }
 
-    
+    void OnCollisionStay(Collision collision)
+    {
+        grounded = true;
+    }
+
+
+
 }
